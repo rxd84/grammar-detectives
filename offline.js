f993bc6cc5856185879cee9085f50f2ffa84fa12
rxd84 <renatoxduque84@gmail.com>
2025-05-13 21:57:57 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1747183890,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/menu_screen_bg1.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
